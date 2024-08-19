48b932a8174973ff9a2c1113c7349473fad0aad7
stopbrother <stopbro0323@naver.com>
2024-08-19 09:03:57 +0900

    solve : 배열의 평균값

--- Introduction/10.js
+++ Introduction/10.js
@@ -1,3 +1,6 @@
+/*정수 배열 numbers가 매개변수로 주어집니다. numbers의 원소의 평균값을 return하도록
+ solution 함수를 완성해주세요. */
+
 function solution(numbers) {
   var answer = 0;
   for (i = 0; i < numbers.length; i++) {
@@ -6,4 +9,5 @@ function solution(numbers) {
   return answer / numbers.length;
 }
 
-console.log(solution([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+// console.log(solution([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])); 5.5
+// console.log(solution([89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99])) 94.0
