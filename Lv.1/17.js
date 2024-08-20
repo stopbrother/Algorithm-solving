a53559904dfbd41009d457ad445d9370e2a4cde9
stopbrother <stopbro0323@naver.com>
2024-08-20 09:56:44 +0900

    solve : 자연수 뒤집어 배열로 만들기

---
+++ Lv.1/17.js
@@ -0,0 +1,29 @@
+/*자연수 n을 뒤집어 각 자리 숫자를 원소로 가지는 배열 형태로 리턴해주세요.
+예를들어 n이 12345이면 [5,4,3,2,1]을 리턴합니다. */
+
+function solution(n) {
+  var answer = [];
+  const arrayInReverse = n.toString().split("").reverse();
+
+  answer = arrayInReverse.map(Number);
+  return answer;
+}
+
+console.log(solution(12345));
+
+/*
+function solution(n) {
+    // 문자풀이
+    // return (n+"").split("").reverse().map(v => parseInt(v));
+
+    // 숫자풀이
+    var arr = [];
+
+    do {
+        arr.push(n%10);
+        n = Math.floor(n/10);
+    } while (n>0);
+
+    return arr;
+}
+*/
