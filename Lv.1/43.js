5fe25295c528ac35df9c8e1568e94d5f5aa446ab
stopbrother <stopbro0323@naver.com>
2025-01-15 19:27:43 +0900

    solve: 크기가 작은 부분문자열

---
+++ Lv.1/43.js
@@ -0,0 +1,40 @@
+/**
+ * 숫자로 이루어진 문자열 t와 p가 주어질 때, t에서 p와 길이가 같은 부분문자열 중에서,
+ *  이 부분문자열이 나타내는 수가 p가 나타내는 수보다 작거나 같은 것이 나오는 횟수를
+ *  return하는 함수 solution을 완성하세요.
+
+예를 들어, t="3141592"이고 p="271" 인 경우, t의 길이가 3인 부분 문자열은
+ 314, 141, 415, 159, 592입니다. 이 문자열이 나타내는 수 중 271보다 작거나 같은 수는
+141, 159 2개 입니다.
+ */
+
+function solution(t, p) {
+  var answer = 0;
+  const lengthP = p.length;
+  const numberP = Number(p);
+
+  for (i = 0; t.length - lengthP >= i; i++) {
+    const sliceT = t.slice(i, lengthP + i);
+    const numberT = Number(sliceT);
+
+    if (numberP >= numberT) answer++;
+  }
+
+  return answer;
+}
+
+console.log("solution", solution("3141592", "271"));
+console.log("solution", solution("500220839878", "7"));
+console.log("solution", solution("10203", "15"));
+
+/*
+function solution(t, p) {
+    let count = 0;
+    for(let i=0; i<=t.length-p.length; i++) {
+        let value = t.slice(i, i+p.length);
+        if(+p >= +value) count++;
+    }
+    return count;
+}
+    숫자로 이러우진 문자열 앞에 +를 붙이면 숫자로 변환됨
+ */
