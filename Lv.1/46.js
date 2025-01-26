17763f4101672f411f8f8444188223436fe045b8
stopbrother <stopbro0323@naver.com>
2025-01-26 15:18:31 +0900

    solve: 숫자 문자열과 영단어

---
+++ Lv.1/46.js
@@ -0,0 +1,56 @@
+/**
+ * 다음은 숫자의 일부 자릿수를 영단어로 바꾸는 예시입니다.
+
+1478 → "one4seveneight"
+234567 → "23four5six7"
+10203 → "1zerotwozero3"
+이렇게 숫자의 일부 자릿수가 영단어로 바뀌어졌거나, 
+혹은 바뀌지 않고 그대로인 문자열 s가 매개변수로 주어집니다.
+s가 의미하는 원래 숫자를 return 하도록 solution 함수를 완성해주세요.
+ */
+
+function solution(s) {
+  const words = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+  ];
+
+  words.forEach((word, index) => {
+    s = s.split(word).join(index);
+  });
+
+  return Number(s);
+}
+
+console.log("solution", solution("one4seveneight"));
+
+/**
+ * "one4seveneight".split("one"); 
+    // 결과: ["", "4seveneight"]
+
+ * ["", "4seveneight"].join(1); 
+    // 결과: "14seveneight"
+
+ */
+
+/**
+ * function solution(s) {
+    let numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    var answer = s;
+
+    for(let i=0; i< numbers.length; i++) {
+        let arr = answer.split(numbers[i]);
+        answer = arr.join(i);
+    }
+
+    return Number(answer);
+}
+ */
